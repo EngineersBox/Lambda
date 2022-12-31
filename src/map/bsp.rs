91f79557ce7f7943d9f34f4b887574c389d9a917
u6940136 <u6940136@anu.edu.au>
2022-12-31 15:44:50 +1100

    Implemented BSP structures

--- src/map/bsp.rs
+++ src/map/bsp.rs
@@ -124,5 +124,52 @@ pub struct Edge {
 }
 
 pub struct Face {
-    
+    pub plane_index: u16,
+    pub plane_size: u16,
+    pub first_edge_index: u32,
+    pub edge_count: u16,
+    pub texture_info: u16,
+    pub styles: [u8; 4], // 0: Lighting styles for the face, 1: Range from 0xFF (dark) to 0x00 (bright), 2: Additional model, 3: Additional model
+    pub lightmap_offset: u32,
+}
+
+pub type SurfaceEdge = i32;
+
+pub struct TextureHeader {
+    pub mip_texture_count: u32,
+}
+
+pub type MipTexOffset = i32;
+
+pub const MAX_TEXTURE_NAME: usize = 16;
+pub const MIP_LEVELS: usize = 4;
+
+pub struct MipTex {
+    pub name: &'static str,
+    pub width: u32,
+    pub height: u32,
+    pub offsets: [u32; MIP_LEVELS],
+}
+
+pub struct TextureInfo {
+    pub s: [f32; 3],
+    pub s_shift: f32,
+    pub t: [f32; 3],
+    pub t_shift: f32,
+    pub mip_tex_index: u32,
+    pub flags: u32,
+}
+
+pub struct Model {
+    pub lower: [f32; 3],
+    pub upper: [f32; 3],
+    pub head_nodes_index: [i32; MAX_MAP_HULLS],
+    pub vis_leaves: i32,
+    pub first_face: i32,
+    pub face_count: i32,
+}
+
+pub struct ClipNode {
+    pub plane_index: i32,
+    pub child_index: [i16; 2],
 }
