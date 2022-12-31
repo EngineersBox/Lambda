78436c99cb321f90964fa559ae2b95c062a90841
u6940136 <u6940136@anu.edu.au>
2022-12-31 14:05:06 +1100

    Added glium dep and created window boilerplate

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+#[macro_use]
+extern crate glium;
+
+fn main() {
+    use glium::glutin;
+    
+    let mut event_loop = glutin::event_loop::EventLoop::new();
+    let window_builder = glutin::window::WindowBuilder::new();
+    let context_builder = glutin::ContextBuilder::new();
+    let display = glium::Display::new(window_builder, context_builder, &event_loop).unwrap();
+    
+    event_loop.run(move |ev, _, control_flow| {
+        let next_frame_time = std::time::Instant::now() +
+            std::time::Duration::from_nanos(16_666_667);
+        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
+        match ev {
+            glutin::event::Event::WindowEvent { event, .. } => match event {
+                glutin::event::WindowEvent::CloseRequested => {
+                    *control_flow = glutin::event_loop::ControlFlow::Exit;
+                    return;
+                },
+                _ => return,
+            },
+            _ => (),
+        }
+    });
+}
