0d15f89f60e28cfcc556909ec97b2ce651b4a21b
u6940136 <u6940136@anu.edu.au>
2022-12-31 14:09:34 +1100

    Added render handle and loop invocation

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
 #[macro_use]
 extern crate glium;
 
+use glium::{glutin, Surface};
+
+fn render(display: &glium::Display) {
+    let mut target = display.draw();
+    target.clear_color(0.0, 0.0, 1.0, 1.0);
+    target.finish().unwrap();
+}
+
 fn main() {
-    use glium::glutin;
-    
     let mut event_loop = glutin::event_loop::EventLoop::new();
     let window_builder = glutin::window::WindowBuilder::new();
     let context_builder = glutin::ContextBuilder::new();
     let display = glium::Display::new(window_builder, context_builder, &event_loop).unwrap();
     
     event_loop.run(move |ev, _, control_flow| {
+
+        render(&display);
+
         let next_frame_time = std::time::Instant::now() +
             std::time::Duration::from_nanos(16_666_667);
         *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
