76faf01a6f7ba7ea7c915a639bfca5c485c53a12
u6940136 <u6940136@anu.edu.au>
2023-01-10 21:01:48 +1100

    Tidied up bsp rendering skeleton and added skeleton camera structure

--- src/map/bsp_renderable.rs
+++ src/map/bsp_renderable.rs
@@ -1,7 +1,9 @@
 use std::boxed::Box;
+use bit_set::BitSet;
 
-use crate::rendering::renderer::{Renderer,Texture,InputLayout,Buffer};
-use crate::rendering::renderable::RenderSettings;
+use crate::rendering::renderer::{Renderer,Texture,InputLayout,Buffer,FaceRenderInfo};
+use crate::rendering::renderable::{Renderable,RenderSettings};
+use crate::rendering::view::camera::Camera;
 use crate::map::bsp::BSP;
 
 pub struct Vertex {
@@ -27,4 +29,52 @@ pub struct BSPRenderable {
     m_decal_vao: Box<dyn InputLayout>,
     vertex_offsets: Vec<usize>,
     faces_drawn: Vec<bool>,
+}
+
+impl BSPRenderable {
+
+    pub fn new(renderer: Box<dyn Renderer>, bsp: Box<BSP>, camera: Box<Camera>) -> Self {
+        todo!()
+    }
+
+    fn load_textures(&self) {
+        todo!()
+    }
+
+    fn load_lightmaps(&self) -> Vec<Vec<glm::Vec2>> {
+        todo!()
+    }
+
+    fn load_sky_textures(&self) {
+        todo!()
+    }
+
+    fn render_skybox(&self) {
+        todo!()
+    }
+
+    fn render_static_geometry(&self, pos: glm::Vec3) -> Vec<FaceRenderInfo> {
+        todo!()
+    }
+
+    fn render_leaf(&self, leaf_index: isize, face_render_info: &Vec<FaceRenderInfo>) {
+        todo!()
+    }
+
+    fn render_bsp(&self, node: isize, vis_list: BitSet<u8>, pos: glm::Vec3, face_render_info: Vec<FaceRenderInfo>) {
+        todo!()
+    }
+
+    fn build_buffers(lm_coords: &Vec<Vec<glm::Vec2>>) {
+        todo!()
+    }
+
+}
+
+impl Renderable for BSPRenderable {
+
+    fn render(settings: &RenderSettings) {
+        todo!()
+    }
+
 }
