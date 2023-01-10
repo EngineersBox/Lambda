92ad0e713b43a12c9952d7362a6248f0e46a49c3
u6940136 <u6940136@anu.edu.au>
2023-01-10 22:58:55 +1100

    Implemented TextureAtlas::alloc_lightap for TextureAtlas::store method

--- src/map/bsp_renderable.rs
+++ src/map/bsp_renderable.rs
@@ -1,4 +1,5 @@
 use std::boxed::Box;
+use std::io::{Result, Error, ErrorKind};
 use bit_set::BitSet;
 
 use crate::rendering::renderer::{Renderer,Texture,InputLayout,Buffer,FaceRenderInfo};
@@ -19,7 +20,7 @@ pub struct VertexWithLM {
 
 pub struct TextureAtlas {
     allocated: Vec<usize>,
-    m_image: Image,
+    pub m_image: Image,
 }
 
 impl TextureAtlas {
@@ -29,6 +30,59 @@ impl TextureAtlas {
             allocated: Vec::new(),
             m_image: Image::from((width, height, channels)),
         };
+    }
+
+    pub fn store(&mut self, image: &Image) -> Result<glm::UVec2> {
+        if image.channels != self.m_image.channels {
+            return Err(Error::new(ErrorKind::InvalidData, format!("Image and atlas channel count mismatch {} != {}", image.channels, self.m_image.channels)));
+        }
+        let loc: Option<glm::UVec2> = self.alloc_lightmap(image.width, image.height);
+        if loc.is_none() {
+            return Err(Error::new(ErrorKind::InvalidData, "Atlas is full"));
+        }
+        let coord: glm::UVec2 = loc.unwrap();
+        for y in 0..image.height {
+            let src: usize = (y * image.width) * image.channels;
+            let dst: usize = ((coord.y as usize + y) * self.m_image.width + coord.x as usize) * image.channels;
+            for i in 0..(image.width * image.channels) {
+                self.m_image.data[dst + i] = image.data[src + i];
+            }
+        }
+        return Ok(coord);
+    }
+
+    pub fn convert_coord(&self, image: &Image, stored_pos: glm::UVec2, coord: glm::Vec2) -> glm::Vec2 {
+        return (glm::vec2(stored_pos.x as f32, stored_pos.y as f32) + coord * glm::vec2(image.width as f32, image.height as f32)) / glm::vec2(self.m_image.width as f32, self.m_image.height as f32);
+    }
+
+    fn alloc_lightmap(&mut self, lm_width: usize, lm_height: usize) -> Option<glm::UVec2> {
+        let mut pos: glm::UVec2 = glm::uvec2(0, 0);
+        let mut best: usize = self.m_image.height;
+        for i in 0..(self.m_image.width - lm_width) {
+            let mut best2: usize = 0;
+            let mut j_result: usize = 0;
+            for j in 0..lm_width {
+                j_result = j;
+                if self.allocated[i + j] >= best {
+                    break;
+                }
+                if self.allocated[i + j] > best2 {
+                    best2 = self.allocated[i + j];
+                }
+            }
+            if j_result == lm_width {
+                pos.x = i as u32;
+                best = best2;
+                pos.y = best as u32;
+            }
+        }
+        if best + lm_height > self.m_image.height {
+            return None;
+        }
+        for i in 0..lm_width {
+            self.allocated[pos.x as usize + i] = best + lm_height;
+        }
+        return Some(pos);
     }
 
 }
