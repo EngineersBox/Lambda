347f8e34826f55216f8dc75bae3f9750903a5838
u6940136 <u6940136@anu.edu.au>
2023-01-11 21:40:59 +1100

    Fixed null termination detection for WAD/BSP element names

--- src/main.rs
+++ src/main.rs
@@ -102,7 +102,7 @@ fn main() {
         crit!(&crate::LOGGER, "Panic at unknown location");
         std::thread::sleep(std::time::Duration::from_millis(1000));
     }));
-    let bsp = map::bsp::BSP::from_file(&"maps/test3.bsp".to_string()).unwrap();
+    let bsp = map::bsp::BSP::from_file(&"maps/crossfire.bsp".to_string()).unwrap();
     std::thread::sleep(std::time::Duration::from_millis(1000));
 
 }
